4b6011ffb8d94aa647cf58f758fc5ab10d0e46d1
sdieselk <sdieselk@gmail.com>
2019-06-07 18:32:48 +0900

    ログインしていない場合のアクセス制限

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,16 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   add_flash_types :success, :info, :warning, :danger
-  helper_method :current_user, :logged_in?  
+  helper_method :current_user, :logged_in?
 
   def current_user
     @current_user ||= User.find_by(id: session[:user_id])
+  end
+
+  def authenticate_user
+    if !logged_in?
+      redirect_to root_path, success: "ログインが必要です"
+    end
   end
 
   def logged_in?
